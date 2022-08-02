e74be892308a6d972bf65a2a8ea66d63cb91e4c9
Jeff Marks <jeff.t.marks@gmail.com>
2022-08-01 22:27:01 -0400

    fixed reset function and refactored code

--- js/index.js
+++ js/index.js
@@ -1,20 +1,31 @@
-const startButton = document.querySelector(".start-button")
-const tournamentDisplay = document.getElementById('tournament')
-const versusDisplay = document.getElementById('versus')
-const vsDancer1 = document.getElementById('vs-dancer-1')
-const vsDancer2 = document.getElementById('vs-dancer-2')
-const vsButton = document.getElementById('vs-button')
-const bracket = document.querySelector('.bracket');
+// ---------- Global Variables ----------
 
-let counter = 1
-let game = 1
+const startBtn = document.getElementById('start-btn');
+const tournamentDisplay = document.getElementById('tournament');
+const vsDisplay = document.getElementById('versus');
+const vsLeft = document.getElementById('vs-left');
+const vsRight = document.getElementById('vs-right');
+const vsBtn = document.getElementById('vs-btn');
+const bracket = document.getElementById('bracket');
 
-let roundOneWinner1;
-let roundOneWinner2;
-let roundOneWinner3;
-let roundOneWinner4;
-let roundTwoWinner1;
-let roundTwoWinner2;
+const div9 = document.getElementById('div-9');
+const div10 = document.getElementById('div-10');
+const div11 = document.getElementById('div-11');
+const div12 = document.getElementById('div-12');
+const div13 = document.getElementById('div-13');
+const div14 = document.getElementById('div-14');
+
+// ---------- Trackers ----------
+
+let counter = 1;
+let game = 1;
+
+let quartersWinner1;
+let quartersWinner2;
+let quartersWinner3;
+let quartersWinner4;
+let semisWinner1;
+let semisWinner2;
 let champ;
 
 let animal1 = {};
@@ -27,181 +38,225 @@ let animal7 = {};
 let animal8 = {};
 
 
-//event listners
+// ---------- Event Listners ----------
 
-//start round event listener
-startButton.addEventListener('click', () => {
-    if (startButton.innerText === 'Start Tournament') {
-        setupR1()
-        startButton.innerText = "Click to Start Round 1"
-    } else if (startButton.innerText === 'Click to Start Round 1') {
+// Start Button to Launch Rounds
+startBtn.addEventListener('click', () => {
+    if (startBtn.innerText === 'Click to Start Tournament') {
+        setupR1();
+        startBtn.innerText = 'Click to Start Round 1';
+    } else if (startBtn.innerText === 'Click to Start Round 1') {
         tournamentDisplay.style.display = 'none';
-        versusDisplay.style.display = 'block';
-        vsDancer1.src = animal1.image_link;
-        vsDancer2.src = animal2.image_link;
-        vsButton.disabled = true;
-    } else if (startButton.innerText === 'Click to Start Round 2') {
+        vsDisplay.style.display = 'block';
+        vsLeft.src = animal1.image_link;
+        vsRight.src = animal2.image_link;
+        vsBtn.disabled = true;
+    } else if (startBtn.innerText === 'Click to Start Round 2') {
         tournamentDisplay.style.display = 'none';
-        versusDisplay.style.display = 'block';
-        vsDancer1.src = roundOneWinner1.image_link;
-        vsDancer2.src = roundOneWinner2.image_link;
-        vsButton.disabled = true;
-    } else if (startButton.innerText === 'Click to Start Championship Round!') {
+        vsDisplay.style.display = 'block';
+        vsLeft.src = quartersWinner1.image_link;
+        vsRight.src = quartersWinner2.image_link;
+        vsBtn.disabled = true;
+    } else if (startBtn.innerText === 'Click to Start Championship Round!') {
         tournamentDisplay.style.display = 'none';
-        versusDisplay.style.display = 'block';
-        vsDancer1.src = roundTwoWinner1.image_link;
-        vsDancer2.src = roundTwoWinner2.image_link;
-        vsButton.disabled = true;
+        vsDisplay.style.display = 'block';
+        vsLeft.src = semisWinner1.image_link;
+        vsRight.src = semisWinner2.image_link;
+        vsBtn.innerText = 'Choose Champion!';
+        vsBtn.disabled = true;
     }
 });
 
-//select winner event listener
-vsDancer1.addEventListener('click', () => {
-    vsDancer1.style.border = '3px solid red';
-    vsDancer2.style.border = '';
-    vsButton.disabled = false;
+// Select Winner (Left & Right)
+
+// Left
+vsLeft.addEventListener('click', () => {
+    vsLeft.style.border = '3px solid #40f022';
+    vsRight.style.border = '';
+    vsLeft.style.opacity = '1';
+    vsRight.style.opacity = '0.2';
+    vsBtn.disabled = false;
+
     if (game === 1) {
-        roundOneWinner1 = animal1
+        quartersWinner1 = animal1;
     } else if (game === 2) {
-        roundOneWinner2 = animal3
+        quartersWinner2 = animal3;
     } else if (game === 3) {
-        roundOneWinner3 = animal5
+        quartersWinner3 = animal5;
     } else if (game === 4) {
-        roundOneWinner4 = animal7
+        quartersWinner4 = animal7;
     } else if (game === 5) {
-        roundTwoWinner1 = roundOneWinner1;
+        semisWinner1 = quartersWinner1;
     } else if (game === 6) {
-        roundTwoWinner2 = roundOneWinner3;
+        semisWinner2 = quartersWinner3;
     } else if (game === 7) {
-        champ = roundTwoWinner1;
-        vsButton.innerText = 'Choose Champion!'
+        champ = semisWinner1;
     }
 })
 
-vsDancer2.addEventListener('click', () => {
-    vsDancer2.style.border = '3px solid red';
-    vsDancer1.style.border = '';
-    vsButton.disabled = false;
+// Right
+vsRight.addEventListener('click', () => {
+    vsRight.style.border = '3px solid #40f022';
+    vsLeft.style.border = '';
+    vsRight.style.opacity = '1';
+    vsLeft.style.opacity = '0.2';
+    vsBtn.disabled = false;
+
     if (game === 1) {
-        roundOneWinner1 = animal2
+        quartersWinner1 = animal2;
     } else if (game === 2) {
-        roundOneWinner2 = animal4
+        quartersWinner2 = animal4;
     } else if (game === 3) {
-        roundOneWinner3 = animal6
+        quartersWinner3 = animal6;
     } else if (game === 4) {
-        roundOneWinner4 = animal8
+        quartersWinner4 = animal8;
     } else if (game === 5) {
-        roundTwoWinner1 = roundOneWinner2;
+        semisWinner1 = quartersWinner2;
     } else if (game === 6) {
-        roundTwoWinner2 = roundOneWinner4;
+        semisWinner2 = quartersWinner4;
     } else if (game === 7) {
-        champ = roundTwoWinner2;
-        vsButton.innerText = 'Choose Champion!';
+        champ = semisWinner2;
     }
 })
 
-//submit winner and next game event listener
-vsButton.addEventListener('click', () => {
-    ++game
-    vsButton.disabled = true;
-    vsDancer1.style.border = '';
-    vsDancer2.style.border = '';
+// Submit Winner and Trigger Next Game
+vsBtn.addEventListener('click', () => {
+    game++;
+    vsBtn.disabled = true;
+    vsLeft.style.border = '';
+    vsRight.style.border = '';
+    vsLeft.style.opacity = '1';
+    vsRight.style.opacity = '1';
+
+
     if (game === 2) {
-        vsDancer1.src = animal3.image_link;
-        vsDancer2.src = animal4.image_link;
+        vsLeft.src = animal3.image_link;
+        vsRight.src = animal4.image_link;
     } else if (game === 3) {
-        vsDancer1.src = animal5.image_link;
-        vsDancer2.src = animal6.image_link;
+        vsLeft.src = animal5.image_link;
+        vsRight.src = animal6.image_link;
     } else if (game === 4) {
-        vsDancer1.src = animal7.image_link;
-        vsDancer2.src = animal8.image_link;
+        vsLeft.src = animal7.image_link;
+        vsRight.src = animal8.image_link;
     } else if (game === 5) {
         tournamentDisplay.style.display = 'block';
-        versusDisplay.style.display = 'none';
-        startButton.innerText = "Click to Start Round 2"
+        vsDisplay.style.display = 'none';
+        startBtn.innerText = 'Click to Start Round 2';
         startR2();
     } else if (game === 6) {
-        vsDancer1.src = roundOneWinner3.image_link;
-        vsDancer2.src = roundOneWinner4.image_link;
+        vsLeft.src = quartersWinner3.image_link;
+        vsRight.src = quartersWinner4.image_link;
     } else if (game === 7) {
         tournamentDisplay.style.display = 'block';
-        versusDisplay.style.display = 'none';
-        startButton.innerText = "Click to Start Championship Round!"
+        vsDisplay.style.display = 'none';
+        startBtn.innerText = 'Click to Start Championship Round!';
         startR3();
     } else if (game === 8) {
-        if (champ === roundTwoWinner1) {
-            vsDancer2.style.display = 'none';
-            vsButton.innerText = 'Play Again?';
-        } else if (champ === roundTwoWinner2) {
-            vsDancer1.style.display = 'none';
-            vsButton.innerText = 'Play Again?';
-            vsButton.disabled = false;
+        if (champ === semisWinner1) {
+            vsRight.style.display = 'none';
+            vsBtn.disabled = false;
+            vsBtn.innerText = 'Play Again?';
+        } else if (champ === semisWinner2) {
+            vsLeft.style.display = 'none';
+            vsBtn.disabled = false;
+            vsBtn.innerText = 'Play Again?';
         }
     } else if (game === 9) {
         tournamentDisplay.style.display = 'block';
-        versusDisplay.style.display = 'none';
+        vsDisplay.style.display = 'none';
         game = 1;
         counter = 1;
-        setupR1()
-        startButton.innerText = "Click to Start Round 1"
+        resetBracket();
+        setupR1();
+        startBtn.innerText = 'Click to Start Round 1';
     }
 })
 
 
+// ---------- Start Round Functions ----------
 
-//start-round functions
 function setupR1() {
     fetch('https://zoo-animal-api.herokuapp.com/animals/rand/8')
     .then(res => res.json())
     .then(dancers => {
-        dancers.forEach(assignDancer)
-        animal1 = dancers[0]
-        animal2 = dancers[1]
-        animal3 = dancers[2]
-        animal4 = dancers[3]
-        animal5 = dancers[4]
-        animal6 = dancers[5]
-        animal7 = dancers[6]
-        animal8 = dancers[7]
+        dancers.forEach(assignDancer);
+        animal1 = dancers[0];
+        animal2 = dancers[1];
+        animal3 = dancers[2];
+        animal4 = dancers[3];
+        animal5 = dancers[4];
+        animal6 = dancers[5];
+        animal7 = dancers[6];
+        animal8 = dancers[7];
     })
 }
 
 function startR2() {
-    document.getElementById('dancer-9').src = roundOneWinner1.image_link
-    document.getElementById('dancer-10').src = roundOneWinner2.image_link
-    document.getElementById('dancer-11').src = roundOneWinner3.image_link
-    document.getElementById('dancer-12').src = roundOneWinner4.image_link
+    const dancer9 = document.createElement('img');
+    dancer9.src = quartersWinner1.image_link;
+    div9.append(dancer9);
+
+    const dancer10 = document.createElement('img');
+    dancer10.src = quartersWinner2.image_link;
+    div10.append(dancer10);
+
+    const dancer11 = document.createElement('img');
+    dancer11.src = quartersWinner3.image_link;
+    div11.append(dancer11);
+
+    const dancer12 = document.createElement('img');
+    dancer12.src = quartersWinner4.image_link;
+    div12.append(dancer12);
 }
 
 function startR3() {
-    document.getElementById('dancer-13').src = roundTwoWinner1.image_link
-    document.getElementById('dancer-14').src = roundTwoWinner2.image_link
+    const dancer13 = document.createElement('img');
+    dancer13.src = semisWinner1.image_link;
+    div13.append(dancer13);
 
+    const dancer14 = document.createElement('img');
+    dancer14.src = semisWinner2.image_link;
+    div14.append(dancer14);
+}
+
+function resetBracket() {
+    div9.firstChild.remove();
+    div10.firstChild.remove();
+    div11.firstChild.remove();
+    div12.firstChild.remove();
+    div13.firstChild.remove();
+    div14.firstChild.remove();
 }
 
 function assignDancer(dancer) {
     document.querySelector(`#dancer-${counter}`).src = dancer.image_link;
-    counter += 1
+    counter++;
 }
+
+
+
+
+
+
 //round 1 handlers
 function assignWinner1(e) {
-    roundOneWinner1 = e.target.src
+    quartersWinner1 = e.target.src
 }
 function assignWinner2(e) {
-    roundOneWinner2 = e.target.src
+    quartersWinner2 = e.target.src
 }
 function assignWinner3(e) {
-    roundOneWinner3 = e.target.src
+    quartersWinner3 = e.target.src
 }
 function assignWinner4(e) {
-    roundOneWinner4 = e.target.src
+    quartersWinner4 = e.target.src
 }
 // round 2 handlers
 function assignWinner5(e) {
-    roundTwoWinner1 = e.target.src
+    semisWinner1 = e.target.src
 }
 function assignWinner6(e) {
-    roundTwoWinner2 = e.target.src
+    semisWinner2 = e.target.src
 }
 //round 3 handler
 function assignWinner6(e) {
